c93ba08774393c96090f279df6aabcc3f2c5c249
Ellen Yan <eyan2024@seas.upenn.edu>
2022-01-02 13:34:55 -0500

    Update settings.py

--- web_project/web_project/settings.py
+++ web_project/web_project/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0#h2sqvzbnrr+eibr5wyk37_+d6wjq&!4koi0-#i^fg_8u(!8r'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
